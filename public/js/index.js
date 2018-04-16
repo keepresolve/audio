b49f41b5b0b818a5848d83aa8a0847ca45fc2fbb
keepresolve <164840613@qq.com>
2018-04-16 23:45:54 +0800

    线上地址

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,6 @@
-	$(function(){
+  $(function(){
     var currentIndex=0
-		// 减少上一曲下一曲的bug
+    // 减少上一曲下一曲的bug
       var ado=document.getElementsByTagName('audio')[0]
      function currentPlay(index){  
          $(".showList li").eq(index).find("mnm, kmlew").css("display","block")
@@ -20,8 +20,8 @@
      })
      // 双击播放
      $("h3").dblclick(function(){
-     	$("audio").get(0).load()
-     	$("audio").get(0).play()
+      $("audio").get(0).load()
+      $("audio").get(0).play()
      })
       // 播放
      $(".big button").eq(0).click(function(){
@@ -29,34 +29,34 @@
      })
      // 暂停
         $(".big button").eq(1).click(function(){
-     	$("audio").get(0).pause()
+      $("audio").get(0).pause()
      })
         // 快进
        $(".big button").eq(2).mousedown(function(){
-     	$("audio").get(0).currentTime+=5;
+      $("audio").get(0).currentTime+=5;
      })
        // 快退
        $(".big button").eq(3).mousedown(function(){
-     	$("audio").get(0).currentTime-=5;
+      $("audio").get(0).currentTime-=5;
      })
        // 音量加
        $(".big button").eq(4).mousedown(function(){
-     	$("audio").get(0).volume+=.1;
+      $("audio").get(0).volume+=.1;
      })
        // 音量减
          $(".big button").eq(5).mousedown(function(){
-     	$("audio").get(0).volume-=.1;
+      $("audio").get(0).volume-=.1;
      })
          //静音
          $(".big button").eq(6).mousedown(function(){
-     	$("audio").get(0).muted=!$("audio").get(0).muted;
+      $("audio").get(0).muted=!$("audio").get(0).muted;
      })
          //全屏
        $(".big button").eq(7).mousedown(function(){
     if( $(".big").width()-0>400){
           $(".big").animate({
-          marginTop:"100px",
-           width:"400px",
+          marginTop:"1rem",
+           width:"4rem",
            height:"60%"
       },function(){
           $(".big button").eq(7).html("全屏")
@@ -74,79 +74,79 @@
      })
      // 下一曲
        $("button").eq(9).mousedown(function(){
-       	var str=null;
+        var str=null;
         currentIndex++
         currentPlay(currentIndex)
             // $(".showList li").each(function(i){
-            // 	if($(this).attr("src")==$("audio").attr("src")){
+            //  if($(this).attr("src")==$("audio").attr("src")){
             //      currentPlay($(this).attr("index")-0+1)
-            // 	}
+            //  }
             // })
        
      })
           
          // 上一曲
       $("button").eq(8).mousedown(function(){
-       	var str=null;
+        var str=null;
             $("ul li").each(function(){
-            	if($(this).text()+".mp3"==$("audio").attr("src")){
-            		  str=$(this).prev().text()+".mp3"
-            	}
+              if($(this).text()+".mp3"==$("audio").attr("src")){
+                  str=$(this).prev().text()+".mp3"
+              }
             })
          $("audio").attr("src",str)
          if(ado.src==""||ado.src==".mp3"){
-      	  ado.src="雨的印记.mp3"
+          ado.src="雨的印记.mp3"
         }
          $("audio").get(0).play()
      })
          // 单曲
       $("button").eq(10).mousedown(function(){
-     	$("audio").attr("loop","loop")
+      $("audio").attr("loop","loop")
      })
       // 顺序
       $("button").eq(11).mousedown(function(){
-     	$("audio").removeAttr("loop")
+      $("audio").removeAttr("loop")
         $("audio").get(0).onended=function(){
-        	var str=null;
-        	  $("ul li").each(function(){
-        	  	 if($("audio").attr("src")==$(this).text()+".mp3"){
+          var str=null;
+            $("ul li").each(function(){
+               if($("audio").attr("src")==$(this).text()+".mp3"){
                       str=$(this).next().text()+".mp3"
-        	  	 }
-        	  })
-        	 $("audio").attr("src",str)
-        	 $("audio").get(0).play()
+               }
+            })
+           $("audio").attr("src",str)
+           $("audio").get(0).play()
         }
      })
          // 随机
         $("button").eq(12).mousedown(function(){
-     	  $("audio").get(0).onended=function(){
-     	  	 var index=Math.ceil(Math.random()*4)
-     	     var str=$("ul li").eq(index).text()+".mp3"
+        $("audio").get(0).onended=function(){
+           var index=Math.ceil(Math.random()*4)
+           var str=$("ul li").eq(index).text()+".mp3"
              $("audio").attr("src",str)
-     	  $("audio").get(0).play()
-     	  }   	   
+        $("audio").get(0).play()
+        }        
      })
         // 循环
          $("button").eq(13).mousedown(function(){
              $("audio").removeAttr("loop")
           $("audio").get(0).onended=function(){
-        	var str=null;
-        	  $("ul li").each(function(){
-        	  	 if($("audio").attr("src")==$(this).text()+".mp3"){
+          var str=null;
+            $("ul li").each(function(){
+               if($("audio").attr("src")==$(this).text()+".mp3"){
                       str=$(this).next().text()+".mp3"
-        	  	 }
-        	  })
-        	  if(str==null||str==".mp3"){
-        	  	str="千千阙歌.mp3"
-        	  }
-        	 $("audio").attr("src",str)
-        	 $("audio").get(0).play()
+               }
+            })
+            if(str==null||str==".mp3"){
+              str="千千阙歌.mp3"
+            }
+           $("audio").attr("src",str)
+           $("audio").get(0).play()
         }
 
          })
          //滚动条
         $("audio").get(0).ontimeupdate=function(){
-        	var num=$("audio").get(0).currentTime/$("audio").get(0).duration
+          var num=$("audio").get(0).currentTime/$("audio").get(0).duration
          $(".progress").get(0).style.width=3*num+"rem"
          $(".font div").get(0).style.top=-3*num+"rem"
           // 显示时间
@@ -156,7 +156,7 @@
             $(".timer span").eq(2).html(ss)
         }
         $(".pro").click(function(e){
-        	 $("audio").get(0).currentTime=e.offsetX/$(".pro").width()*$("audio").get(0).duration
+           $("audio").get(0).currentTime=e.offsetX/$(".pro").width()*$("audio").get(0).duration
         })
 
       //上传文件mp3
