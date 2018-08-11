b424030199e86f2fe12bb0b059240ba853a2434f
caoshiyuan <codesircao@126.com>
2018-08-11 18:59:04 +0800

    加入dockerfile

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/login', function (req, res, next) {
+  if (req.query.password == "caoshiyuan") {
+    res.send({ code: 200, info: "密码正确", src: "https://106.14.114.139:3002/bundle.js" })
+  } else {
+    res.send({ code: 301, info: "密码不对" })
+  }
 
+});
 module.exports = router;
