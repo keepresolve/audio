2a8d99dc93b3e2a1fdcc5789236031198552ad59
keepresolve <164840613@qq.com>
2018-10-16 14:59:21 +0800

    no message

--- jssipWrapper/katalon/mac_run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-pwd=`pwd`
-
-/Applications/Katalon\ Studio.app/Contents/MacOS/katalon --args -noSplash  -runMode=console -projectPath="$pwd/katalon/toolbar_test/toolbar.prj" -retry=0 -testSuitePath="Test Suites/$CASE" -executionProfile="$PROFILE" -browserType="Chrome"
--- jssipWrapper/katalon/win_run.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo "----------------------------"
-REM katalon -noSplash  -runMode=console -consoleLog -noExit -projectPath="D:\current_project\toolbar\katalon\toolbar_test\toolbar.prj" -retry=0 -testSuitePath="Test Suites/toolbar" -executionProfile="env_demo" -browserType="Chrome"
-katalon -noSplash  -runMode=console -consoleLog  -projectPath="%~dp0toolbar_test\toolbar.prj" -retry=0 -testSuitePath="Test Suites/%CASE%" -executionProfile="%NODE_DEMO%" -browserType="Chrome" 
-@echo OK
