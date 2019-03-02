ea99991c84902600b8dc8112f0d1df43505895b2
caoshiyuan <codesircao@126.com>
2019-03-02 17:54:25 +0800

    port

--- panmai/src/database/index.js
+++ panmai/src/database/index.js
@@ -121,6 +121,39 @@ async function createModel() {
             timestamps: false
         }
     )
+    const numerRecord = sequelize.define(
+        'auctionRecord',
+        {
+            //自增id
+            id: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+                primaryKey: true,
+                autoIncrement: true
+            },
+            // 手机号
+            user: {
+                type: Sequelize.text,
+                allowNull: false
+            },
+            //操作
+            operation: {
+                type: Sequelize.text,
+                allowNull: false,
+                defaultValue: 0
+            },
+            createTime: {
+                type: Sequelize.INTEGER,
+                allowNull: true
+            } //企业创建时间
+        },
+        {
+            //使用自定义表名
+            freezeTableName: 'numerRecord',
+            //去掉默认的添加时间和更新时间
+            timestamps: true
+        }
+    )
     var result = await sequelize.sync()
     return result.models
 }
