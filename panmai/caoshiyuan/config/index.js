45969919f35152c1220b2b7c2a1a88c42c1645d7
caoshiyuan <codesircao@126.com>
2019-03-09 20:19:37 +0800

    css

--- panmai/caoshiyuan/config/index.js
+++ panmai/caoshiyuan/config/index.js
@@ -36,7 +36,7 @@ module.exports = {
         // https://vue-loader.vuejs.org/en/options.html#cachebusting
         cacheBusting: true,
 
-        cssSourceMap: true
+        cssSourceMap: false
     },
 
     build: {
@@ -52,7 +52,7 @@ module.exports = {
          * Source Maps
          */
 
-        productionSourceMap: true,
+        productionSourceMap: false,
         // https://webpack.js.org/configuration/devtool/#production
         // devtool: '#source-map',
         devtool: false,
